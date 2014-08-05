336853f909e2bdb6e116d810a95ff7e8ed702f86
Jordan Kanter <jikanter@gmail.com>
2014-08-05 16:47:20 -0500

    initial commit

---
+++ config.php
@@ -0,0 +1,17 @@
+<?php
+// CHANGE THIS TO <my_url>/xmlrpc.php 
+$endpoint = "http://localhost/~admin/2014/7/5/BoomShaka/xmlrpc.php";
+// CHANGE THIS TO YOUR ADMIN USERID
+$username = "admin";
+// CHANGE THIS TO YOUR ADMIN PASSWORD
+$password = "admin";
+// post id 3 = Contact Page
+// post id 4 = Bio Page
+// post id 5 = Archive 
+// other posts tbd (or products)
+$artist_posts = array(
+	'contact' => 3,
+	'bio' => 4,
+	'archive' => 5
+);
+?>
