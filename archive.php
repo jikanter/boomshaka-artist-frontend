a76005e8606f26f2e70c59636039b5854f599cd4
Jordan Kanter <jikanter@gmail.com>
2014-08-17 20:16:09 -0500

    made archive.php a little more understandable. added a non-working implementation to piece.

--- archive.php
+++ archive.php
@@ -1,9 +1,16 @@
 <?php
-/* test xml app
- */
+/* test xml app - Retrieve the piece archive */
+
+class ArchiveFilter { 
+  var $post_type = "piece";
+  var $post_status = "publish"; // only the published posts
+}
+
+$archive = new ArchiveFilter;
+
 require_once('config.php');
 $post_id = $artist_posts['archive']; // the archive page
-$request = xmlrpc_encode_request("wp.getPost", array(0, $username, $password, $post_id));
+$request = xmlrpc_encode_request("wp.getPosts", array(0, $username, $password, $archive));
 $context = stream_context_create(array('http' => array(
 								       'method' => 'POST', 
 									   'header' => 'Content-Type: text/xml', 
@@ -20,17 +27,21 @@ else {
 		print_r($response);
 		echo("<pre>");
 	}
-	echo("<h1>{$response[post_title]}</h1>");
-	if ($_GET['flash'] != '') {  
-		echo("<h2 style='color: green;'>" . $_GET['flash'] . "</h2>");
-	}
-	echo("<form id='artist-contact-info' method='POST' enctype='application/x-www-form-urlencoded' action='update-post.php'>");
-	echo("<textarea form='artist-archive-info' name='content' rows='20' cols='200'>");
-	echo("{$response[post_content]}");
-	echo("</textarea>");
-	echo("<input name='post_type' value='archive' type='hidden' />");
-	echo("<input name='post_id' value='${post_id}' type='hidden' />");
-	echo("<input value='Update Post' type='submit' />");
-	echo("</form>");
+  // will not have a {$response[post_title]} because this is a list of posts
+  for ($i = 0; $i < sizeof($response); $i++) {
+    $response_content_item = $response[$i];
+    echo("<h1>{$response_content_item[post_title]}</h1>");
+    if ($_GET['flash'] != '') {  
+    	echo("<h2 style='color: green;'>" . $_GET['flash'] . "</h2>");
+    }
+    echo("<form id='artist-contact-info' method='POST' enctype='application/x-www-form-urlencoded' action='update-post.php'>");
+    echo("<textarea form='artist-archive-info' name='content' rows='20' cols='200'>");
+    echo("{$response_content_item[post_content]}");
+    echo("</textarea>");
+    echo("<input name='post_type' value='archive' type='hidden' />");
+    echo("<input name='post_id' value='{$response_content_item[post_id]}' type='hidden' />");
+    echo("<input value='Update Post' type='submit' />");
+    echo("</form>");
+  }
 }
 ?>
---
+++ piece.php
@@ -0,0 +1,52 @@
+<?php
+require_once('config.php');
+// sanitize the post_id. make sure 
+$post_id = preg_replace('/[^0-9]/', '', $_GET['post_id']);
+$request = xmlrpc_encode_request("wp.getPost", array(0, $username, $password, $post_id));
+$context = stream_context_create(array('http' => array(
+								       'method' => 'POST', 
+									   'header' => 'Content-Type: text/xml', 
+									   'content' => $request)));
+$data = file_get_contents($endpoint, false, $context);
+$response = xmlrpc_decode($data);
+if ($response && xmlrpc_is_fault($response)) { 
+	trigger_error("xmlrpc: {$response[faultString]}, {$response[faultCode]}");
+} 
+else { 
+	echo("<!DOCTYPE HTML>");
+	if (defined('WP_DEBUG')) { 
+		echo("<pre>");
+		print_r($response);
+		echo("<pre>");
+	}
+	echo("<h1>{$response[post_title]}</h1>");
+	if ($_GET['flash'] != '') {  
+		echo("<h2 style='color: green;'>" . $_GET['flash'] . "</h2>");
+	}
+	echo("<form id='artist-piece-info' method='POST' enctype='application/x-www-form-urlencoded' action='update-post.php'>");
+	echo("<textarea form='artist-piece-info' name='content' rows='20' cols='200'>");
+	echo("{$response[post_content]}");
+	echo("</textarea>");
+  echo("<section>");
+  echo("<label for='piece_price'>Price:</label>");
+  echo("<input name='piece_price' type='text' />");
+  echo("</section>");
+  echo("<section>");
+  echo("<label for='piece_width'>Width:</label>");
+  echo("<input name='piece_width' type='text' />");
+  echo("</section>");
+  echo("<section>");
+  echo("<label for='piece_height'>Height:</label>");
+  echo("<input name='piece_height' type='text' />");
+  echo("</section>");
+  echo("<section>");
+  echo("<label for='piece_length'>Length:</label>");
+  echo("<input name='piece_length' type='text' />");
+  echo("</section>");
+  echo("<br />"); // get rid of this
+	echo("<input value='Update Post' type='submit' />");
+	echo("<input name='post_type' value='bio' type='hidden' />");
+	echo("<input name='post_id' value='${post_id}' type='hidden' />");
+	echo("</form>");
+}
+?>
