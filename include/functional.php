97ed1e11559380e5ef1d772474bc7842937c54a5
Jordan Kanter <jikanter@gmail.com>
2014-10-16 16:55:33 -0500

    minor functional library update.

--- include/functional.php
+++ include/functional.php
@@ -3,16 +3,21 @@
 // Boomshaka functional programming library, inspired by pharen (http://pharen.org)
 
 class Boom_Functional { 
-  public $functions = array();
+  public static $_root = FALSE;
+  public static $_current = array( 'node' => FALSE, 'env' => FALSE);
+  public static $functions = array();
 }
 
-function defGeneric($fun) { 
-  $Boom_Functional->functions->push($fun);
+function defGeneric($fun, $args = array()) { 
+  if (sizeof($args) > 0) { 
+    Boom_Functional::$functions[] = array($fun => $args);
+  }
+  return Boom_Functional::$functions[] = $fun;
 }
 
 function funCall($fun, $args) { 
   // call the function with name $fun with args $args
-  call_user_func_array($fun, $args);
+  call_user_func_array(__NAMESPACE__ . 'Boom_Functional::$functions[' . $fun . ']', $args);
 }
 
 ?>
