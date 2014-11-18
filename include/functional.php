85a797179fa3609c6284fe8afa562c94db682f96
Jordan Kanter <jikanter@gmail.com>
2014-11-17 21:51:41 -0600

    small changes to functional.php

--- include/functional.php
+++ include/functional.php
@@ -5,7 +5,18 @@
 class Boom_Functional { 
   public static $_root = FALSE;
   public static $_current = array( 'node' => FALSE, 'env' => FALSE);
+  public static $_next = array(    'node' => FALSE, 'env' => FALSE);
   public static $functions = array();
+  
+  public function boomParse($node, $env = array(ENV)) { 
+    if ($_current['node'] != FALSE) { 
+      $_current['node'] = $node;
+      $_current['env'] = $env;
+    }
+    else { 
+      
+    }
+  }
 }
 
 function defGeneric($fun, $args = array()) { 
@@ -19,5 +30,6 @@ function funCall($fun, $args) {
   // call the function with name $fun with args $args
   call_user_func_array(__NAMESPACE__ . 'Boom_Functional::$functions[' . $fun . ']', $args);
 }
+
 
 ?>
